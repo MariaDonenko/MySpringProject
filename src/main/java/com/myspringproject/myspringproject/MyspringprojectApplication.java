35942cc4d1d0540ce36146bed2fd8556bb02a905
Maria Donenko <mariadonenko@gmail.com>
2018-09-07 22:16:50 +0300

    Initial commit

---
+++ src/main/java/com/myspringproject/myspringproject/MyspringprojectApplication.java
@@ -0,0 +1,12 @@
+package com.myspringproject.myspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringprojectApplication.class, args);
+	}
+}
