55e91c0913d65255b021bf348db7eabd0d8cd58e
Maria Donenko <mariadonenko@gmail.com>
2018-09-08 07:53:24 +0300

    Add equals and toString to Entities

--- src/main/java/com/myspringproject/myspringproject/model/Author.java
+++ src/main/java/com/myspringproject/myspringproject/model/Author.java
@@ -1,6 +1,7 @@
 package com.myspringproject.myspringproject.model;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import javax.persistence.*;
 
@@ -63,4 +64,27 @@ import javax.persistence.*;
             this.books = books;
         }
 
-}
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Author author = (Author) o;
+            return Objects.equals(id, author.id);
+        }
+
+        @Override
+        public int hashCode() {
+
+            return Objects.hash(id);
+        }
+
+        @Override
+        public String toString() {
+            return "Author{" +
+                    "id=" + id +
+                    ", firstName='" + firstName + '\'' +
+                    ", lastName='" + lastName + '\'' +
+                    ", books=" + books +
+                    '}';
+        }
+    }
